f875130b2dd32e29d55de9a56a3b2b1fc8306a4a
viverbungag <viverbungag1@gmail.com>
2022-02-16 05:01:25 +0800

    finished JavaScript Fundamentals Part 1

---
+++ assignments.js
@@ -0,0 +1,13 @@
+// Values and Variables
+
+// let country = "Philippines";
+// let continent = "Asia";
+// let population = 109.6;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//---------------------------------------------------------------------------------------------------------//
+
+// Data Types
---
+++ codingChallenge.js
@@ -0,0 +1,81 @@
+//----------------------------------Coding Challenge 1----------------------------------//
+
+// // Data 1
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// // Data 2
+// const markWeight = 95;
+// const markHeight = 1.88;
+// const johnWeight = 85;
+// const johnHeight = 1.76;
+
+// const markBMI =  markWeight / (markHeight * markHeight);
+// const johnBMI = johnWeight / (johnHeight * johnHeight);
+
+// const markHigherBMI = markBMI > johnBMI;
+
+
+//----------------------------------Coding Challenge 2----------------------------------//
+
+// // Data 1
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// // Data 2
+// const markWeight = 95;
+// const markHeight = 1.88;
+// const johnWeight = 85;
+// const johnHeight = 1.76;
+
+// const markBMI =  markWeight / (markHeight * markHeight);
+// const johnBMI = johnWeight / (johnHeight * johnHeight);
+
+// if (markBMI > johnBMI){
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// }else{
+//     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+// }
+
+
+//----------------------------------Coding Challenge 3----------------------------------//
+
+// //Data 1
+// const dolphins = (96 + 108 + 89) / 3;
+// const koalas = (88 + 91 + 110) / 3;
+
+// //Data Bonus 1
+// const dolphins = (97 + 112 + 101) / 3;
+// const koalas = (109 + 95 + 123) / 3;
+
+// //Data Bonus 2
+// const dolphins = (97 + 112 + 101) / 3;
+// const koalas = (109 + 95 + 106) / 3;
+
+// if (dolphins >= 100 && koalas >= 100){
+//     if(dolphins > koalas){
+//         console.log("Dolhpins is the winner");
+//     }else if(dolphins === koalas){
+//         console.log("It is a draw");
+//     }else{
+//         console.log("Koalas is the winner");
+//     }
+// }else{
+//     console.log("There are no winners");
+// }
+
+
+
+//----------------------------------Coding Challenge 4----------------------------------//
+
+
+// const bill = 275;
+// const bill = 40;
+// const bill = 430;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
